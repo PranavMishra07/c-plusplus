fa1c24daab0f1e429a846b61145b63135b9e9956
PranavMishra07 <pranav072003@gmail.com>
2024-12-10 14:21:39 +0530

    k

---
+++ Exception.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"\nLine 1 :";
+	cout<<"\nLine 2 :";
+	cout<<"\nLine 3 :";
+	cout<<"\nLine 4 :";
+	int c=10/0;
+
+	cout<<"\nLine 5 :";
+	cout<<"\nLine 6 :";
+	cout<<"\nLine 7 :";
+	cout<<"\nLine 8 :";
+	cout<<"\nLine 9 :";
+	cout<<"\nLine 10 :";
+	cout<<"\nLine 11 :";
+
+}
