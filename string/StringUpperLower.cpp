1f53efb685d6d8ef0b2a7850eebd20d49295b419
PranavMishra07 <pranav072003@gmail.com>
2025-04-15 19:45:34 +0530

    Update StringUpperLower.cpp

--- string/StringUpperLower.cpp
+++ string/StringUpperLower.cpp
@@ -46,3 +46,26 @@ int main(){
 	
 	return 0;
 }
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	string s;
+	cout<<"Enter Any String : ";
+	getline(cin,s);
+	cout<<"String Before Lower Case : "<<s;
+	string x="";
+	char c;
+	for(int i=0;i<s.length();i++){
+		c=s.at(i);
+		if(c>=65 && c<=96){
+			c=c+32;
+			x.push_back(c);
+		}
+		else{
+			x.push_back(c);
+		}
+	}
+	cout<<"\nString In Lower Case : "<<x;
+	
+	return 0;
+}
